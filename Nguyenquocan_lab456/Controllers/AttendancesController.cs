3969afedbda8d9f25b75e32ab93cedae3268fb16
Quoc An <quocan393@gmail.com>
2020-06-14 19:35:43 +0700

    https://github.com/quocan39/quocan393.git

---
+++ Nguyenquocan_lab456/Controllers/AttendancesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Nguyenquocan_lab456.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+    }
+}
+
