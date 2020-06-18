6737e43a97d37e4ca6674b5ac328198b3b2cf808
quocan <Administrator@PC239>
2020-06-18 11:11:38 +0700

    Doem danh ngay 18/06/2020

--- Nguyenquocan_lab456/Models/ApplicationUser.cs
+++ Nguyenquocan_lab456/Models/ApplicationUser.cs
@@ -27,7 +27,7 @@ namespace Nguyenquocan_lab456.Models
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie); 
             // Add custom user claims here
             return userIdentity;
         }
